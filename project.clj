e965561ea359a0b6e63a0e73fb30132b8cf1c7e7
Michael Bianco <iloveitaly@gmail.com>
2020-03-06 10:46:44 -0700

    clj-http properly downloading

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :main ^:skip-aot feedbin.core
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+  [clj-http "3.10.0"]]
   :repl-options {:init-ns feedbin.core})
